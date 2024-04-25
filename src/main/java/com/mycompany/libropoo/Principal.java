220685fab14d5b2bd5dd207a39c68f4a5c7e37c6
sofia <x@gmail.com>
2024-04-25 09:50:36 -0500

    se subió el código para la solución del taller propuesto por el docente.

--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,20 @@
 package com.mycompany.libropoo;
-
+import java.util.Scanner;
 public class Principal {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Indique el año de publicación del libro: ");
+        int annioPublicacion = sc.nextInt();
+        System.out.println("Escriba el titulo del libro: ");
+        sc.nextLine();
+        String titulo = sc.nextLine();
+        System.out.println("Escriba el nombre del autor del libro: ");
+        String autor = sc.nextLine();
+        System.out.println("Indique el genero del libro: ");
+        String genero = sc.nextLine();
+        boolean leido = false;
+        Libro libro1 = new Libro(annioPublicacion,titulo,autor,genero,leido);
+        libro1.marcarLeido();
+        System.out.println(libro1.mostrarInformación() + "\n" + (libro1.getLeido()==true?"El libro ya fue leido":"El libro no ha sido leido") + "\n" + (libro1.esAntiguo()==true?"Este libro es antiguo":"Este libro es de un año reciente"));
+    }
 }
